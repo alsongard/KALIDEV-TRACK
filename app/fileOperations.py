9951ca251c8d81ac4c01aad0c208203f89f50126
alsongard <alsongard@gmail.com>
2025-10-22 12:37:47 +0300

    new function for creating folder and pathExists

--- app/fileOperations.py
+++ app/fileOperations.py
@@ -11,39 +11,45 @@ class ReadWriteUpdateDeleteFileOperations():
         home_dir = os.path.expanduser("~")
         app_project_folder = home_dir+"/"+"PROJECTS"
 
+        if os.path.exists(app_project_folder):
+            print("path exists")
+            user_projects_folders = os.listdir(app_project_folder)
+            # check if folder:
+            for item in user_projects_folders:
+                if not os.path.isfile(os.path.join(app_project_folder,item)): # item is not a file continue
+                    
+                    # set project path
+                    project_folder_path  = os.path.join(app_project_folder,item)
+                    # print(f"project_folder_path: {project_folder_path}")
+                    
+                    # list files and directories
+                    get_all_files_dirs = os.listdir(os.path.join(app_project_folder,item))
+                    # print(get_all_files_dirs)
+                    for item in get_all_files_dirs:
+                        if os.path.isfile(os.path.join(project_folder_path, item)):
+                            # print(f"Found file: {item}")
+                            
+                            # adding functionality to read only project_data.json
+                            # Found file: project_data.json
+                            # Found file: project_task.json
+                            if item == "project_data.json":
+                                # print('Success in founding project_data.json') # testing:working
+                                # read the data in the file: json files
+                                with open(os.path.join(project_folder_path, item), "r") as file:
+                                    data = json.load(file)
+                                    # print('Data is')
+                                    # print(data)
+                                    self.data_array.append(data)
+            # print(self.data_array)
+            return self.data_array
+        else: 
+            return self.data_array
         # list everything in folder:~/username/PROJECTS
-        user_projects_folders = os.listdir(app_project_folder)
 
-
-        # check if folder:
-        for item in user_projects_folders:
-            if not os.path.isfile(os.path.join(app_project_folder,item)): # item is not a file continue
-                
-                # set project path
-                project_folder_path  = os.path.join(app_project_folder,item)
-                # print(f"project_folder_path: {project_folder_path}")
-                
-                # list files and directories
-                get_all_files_dirs = os.listdir(os.path.join(app_project_folder,item))
-                # print(get_all_files_dirs)
-                for item in get_all_files_dirs:
-                    if os.path.isfile(os.path.join(project_folder_path, item)):
-                        # print(f"Found file: {item}")
-                        
-                        # adding functionality to read only project_data.json
-                        # Found file: project_data.json
-                        # Found file: project_task.json
-                        if item == "project_data.json":
-                            # print('Success in founding project_data.json') # testing:working
-                            # read the data in the file: json files
-                            with open(os.path.join(project_folder_path, item), "r") as file:
-                                data = json.load(file)
-                                # print('Data is')
-                                # print(data)
-                                self.data_array.append(data)
-        # print(self.data_array)
-        return self.data_array
-                
+    def createProjectFolder(self):
+        home_director = os.path.expanduser("~")
+        os.mkdir(home_director+"/" + "PROJECTS")     
+        return 0           
 # read_instance = ReadWriteUpdateDeleteFileOperations()
 # # print(read_instance.readFiles())
 # geazy = read_instance.readFiles()
